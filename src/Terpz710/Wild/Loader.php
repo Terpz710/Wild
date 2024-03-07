41e2f76dfe0b849816264a3bb594a36f1b596c2b
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-03-06 22:56:39 -0800

    rixes

--- src/Terpz710/Wild/Loader.php
+++ src/Terpz710/Wild/Loader.php
@@ -5,14 +5,23 @@ declare(strict_types=1);
 namespace Terpz710\Wild;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 
 use Terpz710\Wild\Command\WildCommand;
 
 class Loader extends PluginBase
 {
+    /** @var Config */
+    private $config;
+
     public function onEnable(): void
     {
         $this->saveDefaultConfig();
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML); // Initialize $config
         $this->getServer()->getCommandMap()->register("wild", new WildCommand($this));
+    }
+
+    public function getConfig(): Config {
+        return $this->config;
     }
 }
